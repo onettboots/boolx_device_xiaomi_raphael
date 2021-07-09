4bbb21d67900315650e1431f5045c79792d303a3
nato6613 <normandija1945@gmail.com>
2021-07-09 14:36:56 +0000

    raphael: Add pixel_2016_exclusive.xml
    * This allows us to use unlimited backup in Google Photos infinitely.
    
    Former-commit-id: 4070a2a3830344224bd70a3105a56ca17ed05002
    Signed-off-by: SageOfD6Path <mai2anirban95@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -169,6 +169,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true \
