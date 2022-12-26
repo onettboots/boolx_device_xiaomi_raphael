37caa8bcce3cab12dbd03ed04157ce917e3d47ce
onettboots <blackcocopet@gmail.com>
2022-12-26 12:57:49 +0000

    Revert "raphael: Build PalyrimDoze"
    This reverts commit e26e3775b648bcf37f9a32b4134ccee3789f912c.

--- device.mk
+++ device.mk
@@ -554,7 +554,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
-
-# Palyrim Doze
-PRODUCT_PACKAGES += \
-   PalyrimDoze
