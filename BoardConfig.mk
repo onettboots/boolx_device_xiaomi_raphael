PixelExperience-Devices/device_xiaomi_raphael

raphael: Checkout NFC to LOS sm8150-common tree
* From https://github.com/LineageOS/android_device_xiaomi_sm8150-common.

Revert "raphael: build some NFC packages"

This reverts commit bbee1443e188cd5cfb08c25bcb26deebe920d652.

BoardConfig.mk
@@ -77,12 +77,15 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
-ODM_MANIFEST_SKUS += raphael
-ODM_MANIFEST_RAPHAEL_FILES := $(DEVICE_PATH)/hidl/manifest_nfc.xml
+ODM_MANIFEST_SKUS += nfc nfc_ese
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/hidl/manifest_nfc.xml
+ODM_MANIFEST_NFC_ESE_FILES := \
+    $(DEVICE_PATH)/hidl/manifest_nfc.xml \
+    $(DEVICE_PATH)/hidl/manifest_ese.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
-TARGET_RECOVERY_DEVICE_MODULES := libinit_raphael
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_raphael
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_raphael
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000

onettboots/boolx_device_xiaomi_raphael

Signed-off-by: onettboots <blackcocopet@gmail.com>

BoardConfig.mk
@@ -112,8 +112,11 @@ DEVICE_MATRIX_FILE := \
 ODM_MANIFEST_FILES += \
     $(DEVICE_PATH)/vintf/manifest-qva.xml
 
-ODM_MANIFEST_SKUS += raphael
-ODM_MANIFEST_RAPHAEL_FILES := $(DEVICE_PATH)/vintf/manifest_nfc.xml
+ODM_MANIFEST_SKUS += nfc nfc_ese
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/vintf/manifest_nfc.xml
+ODM_MANIFEST_NFC_ESE_FILES := \
+    $(DEVICE_PATH)/vintf/manifest_nfc.xml \
+    $(DEVICE_PATH)/vintf/manifest_ese.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000
@@ -220,8 +223,8 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include device/xiaomi/raphael/sepolicy/sepolicy.mk
 
 # Vendor init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
-TARGET_RECOVERY_DEVICE_MODULES := libinit_raphael
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_raphael
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_raphael
 
 # USB
 TARGET_QTI_USB_SUPPORTS_AUDIO_ACCESSORY := true